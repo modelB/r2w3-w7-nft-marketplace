cf1de3bfe075f3932d82e6fc4514c199fd30f248
Brian Grosso <bgro63@gmail.com>
2022-11-13 20:01:51 -0500

    update take2

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,19 +4,25 @@ const fs = require("fs");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
+  console.log("deployer obtained");
   const balance = await deployer.getBalance();
+  console.log("balance obtained");
   const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  console.log("Marketplace factory established");
   const marketplace = await Marketplace.deploy();
+  console.log("marketplace instance created");
 
   await marketplace.deployed();
+  console.log("marketplace deployed");
 
   const data = {
     address: marketplace.address,
-    abi: JSON.parse(marketplace.interface.format('json'))
-  }
+    abi: JSON.parse(marketplace.interface.format("json")),
+  };
 
   //This writes the ABI and address to the mktplace.json
-  fs.writeFileSync('./src/Marketplace.json', JSON.stringify(data))
+  fs.writeFileSync("./src/Marketplace.json", JSON.stringify(data));
+  console.log("deploy finished");
 }
 
 main()
